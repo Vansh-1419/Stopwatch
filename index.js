f8a26cedaae88fe91281110a70a0c1edef0b0e5d
Vansh Tandon <Vanshtandon1423@gmail.com>
2022-02-06 11:51:08 +0530

    stop button working

--- index.js
+++ index.js
@@ -2,42 +2,60 @@ const time = document.querySelector(".time");
 const seconds = document.querySelector(".seconds");
 const minutes = document.querySelector(".minutes");
 const hours = document.querySelector(".hours");
-const start = document.querySelector("#Start");
+const start = document.querySelector("#start");
+const stopl = document.querySelector("#stop");
 let second = `${0}`;
-let minute = 0;
 let mint = 0;
+let hr = `${0}`;
 
-//seconds
 const sec = (second) => {
   setTimeout(() => {
-    seconds.textContent = second;
-    console.log(seconds.textContent);
+    seconds.textContent = `${second}`;
   }, 1000 * second);
 };
-//minutes
-const min = (minute) => {
-  setTimeout(() => {
-    second = 0;
-    seconds.textContent = 0;
-    minutes.textContent = minute;
-    console.log(seconds.textContent);
-  }, 6000 * minute);
-};
 
-start.addEventListener("click", () => {
-  if (second < 10) {
-    while (second <= 10) {
+const min = (mint) => {
+  setTimeout(() => {
+    second = `${0}`;
+    seconds.textContent = second;
+    while (second <= 60) {
       sec(second);
       second++;
     }
-    if ((second = 10)) {
-      second = 0;
-      while (minute <= 10 && second <= 10) {
-        min(minute);
-        minute++;
-        sec(second);
-        second++;
-      }
+
+    minutes.textContent = mint;
+  }, 60000 * mint);
+};
+const hour = (hr) => {
+  setTimeout(() => {
+    mint = `${0}`;
+    minutes.textContent = mint;
+    while (mint <= 60) {
+      min(mint);
+      mint++;
+    }
+    hours.textContent = hr;
+  }, 60000 * 60 * hr);
+};
+
+start.addEventListener("click", () => {
+  if (second <= 60) {
+    while (second <= 60) {
+      sec(second);
+      second++;
+    }
+  }
+  if (second > 60) {
+    while (mint <= 60) {
+      min(mint);
+      mint++;
+    }
+  }
+  if (mint > 60) {
+    while (hr <= 60) {
+      hour(hr);
+      hr++;
     }
   }
 });
+stopl.addEventListener("click", () => {});
